df1b35e81461b8a8bb74212e9d544beba78f0b7d
KendraAnderson <kor04003@byui.edu>
2022-05-23 14:12:49 -0700

    edits

--- src/js/notices.js
+++ src/js/notices.js
@@ -1,16 +1,20 @@
+import { convertToJson } from "./utils";
 const eventUrl = "https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=";
 const key = "KFPsJ58iSGGzrFPIlYncQiDxUMsChaZ1osGtSUj3";
 
-//export default class Notices {
-//    constructor()
-//}
-
-async function getNotices() {
-    return await fetch(`${eventUrl}${key}`)
-        .then(response => response.json())
-        .then(data => data.events[0])
-        .catch(error => console.log(error));
+export default class Notices {
+    async getNotices() {
+        return await fetch (`${eventUrl}${key}`)
+        .then(res => convertToJson(res));
+    }
 }
 
-const notice = getNotices();
-console.log(notice);
+// async function getNotices() {
+//     return await fetch(`${eventUrl}${key}`)
+//         .then(response => response.json())
+//         .then(data => data.events[0])
+//         .catch(error => console.log(error));
+// }
+
+ const notice = getNotices();
+ console.log(notice);
