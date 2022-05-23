0eb51b12c78678ec2a09f389a13cdb405619db94
KendraAnderson <kor04003@byui.edu>
2022-05-23 14:49:33 -0700

    fetch working

--- src/js/notices.js
+++ src/js/notices.js
@@ -1,20 +1,23 @@
-import { convertToJson } from "./utils";
+//import { convertToJson } from "./utils";
 const eventUrl = "https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=";
 const key = "KFPsJ58iSGGzrFPIlYncQiDxUMsChaZ1osGtSUj3";
 
-export default class Notices {
-    async getNotices() {
-        return await fetch (`${eventUrl}${key}`)
-        .then(res => convertToJson(res));
-    }
-}
-
-// async function getNotices() {
-//     return await fetch(`${eventUrl}${key}`)
-//         .then(response => response.json())
-//         .then(data => data.events[0])
-//         .catch(error => console.log(error));
+// export default class Notices {
+//     async getNotices() {
+//         return await fetch (`${eventUrl}${key}`)
+//         .then(res => convertToJson(res))
+//         .then(result =>);
+//     }
 // }
 
- const notice = getNotices();
- console.log(notice);
+async function getNotices() {
+    return await fetch(`${eventUrl}${key}`)
+        .then(response => response.json())
+        .then(data => data.events[0])
+        .catch(error => console.log(error));
+}
+
+//testing
+//const thing = new Notices();
+const notice = getNotices();
+console.log(notice);
