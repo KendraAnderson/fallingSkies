432caf0d71621a45a3e5b43d4334f7f21063c08e
baajayi <delly31@hotmail.com>
2022-05-25 10:17:43 +0100

    headerfootertemplate script

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { loadHeaderFooter } from "./utils";
 //Call displayHeaderFooter
-
+loadHeaderFooter("header", "footer")
 //Call other functions to run program...
--- src/js/utils.js
+++ src/js/utils.js
@@ -6,3 +6,33 @@ export async function convertToJson(res) {
       throw { name: "servicesError", message: jsonResponse };
     }
   }
+
+  export async function renderWithTemplate(
+    template,
+    parentElement,
+    data,
+    callback
+  ) {
+    await template;
+    let clone = template.content.cloneNode(true);
+    if (callback) {
+      clone = callback(clone, data);
+    }
+    parentElement.appendChild(clone);
+  }
+
+  export async function loadTemplate(path) {
+    const templateData = await fetch(path).then((response) => response.text());
+    let newTemplate = document.createElement("template");
+    newTemplate.innerHTML = templateData;
+    return newTemplate;
+  }
+
+  export async function loadHeaderFooter(header, footer) {
+    let headerTemplate = await loadTemplate("./partials/header.html");
+    let footerTemplate = await loadTemplate("./partials/header.html");
+    let headerElement = document.querySelector(header);
+    let footerElement = document.querySelector(footer);
+    renderWithTemplate(headerTemplate, headerElement);
+    renderWithTemplate(footerTemplate, footerElement);
+  }
